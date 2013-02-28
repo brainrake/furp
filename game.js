5e66c6ae932e593053818817d4e1c5a7a2a26575
Márton Boros <martonboros@gmail.com>
2013-02-28 08:42:39 +0100

    game up

--- game.js
+++ game.js
@@ -1,201 +1,49 @@
 (function(){
-  var __, getctx, inner, genOp, generalize_operator, Tensor, ref$, BLOCKSIZE, ACCEL, PLAYER_SIZE, GRAVITY, MAP, INITIAL_SPEED, INITIAL_POSITION, box, draw, collide, collideBlocks, controls, acceleration, velocity, position, player;
+  var __, ACCEL, PLAYER_SIZE, GRAVITY, MAP, INITIAL_SPEED, INITIAL_POSITION, vadd, vsub, vlen, vdot, vangle, vnorm, draw, collide, collideBlocks, time, controls, acceleration, velocity, position, collision, x$;
   import$(this, furp);
   __ = function(it){
     console.log.apply(console, arguments);
     return it;
   };
-  getctx = function(size){
-    var el, ctx;
-    if (!$('#__signals')[0]) {
-      $('body').append("<div id='__signals' style='position:fixed;height:100%;width:100%;'></div>");
-    }
-    $('#__signals').append(el = $("<canvas height='" + size + "' width='" + size + "' style='float:right;top:0;left:0;height:" + size + "px;width:" + size + "px;'>"));
-    return ctx = el[0].getContext('2d');
-  };
-  furp.SignalClass.prototype.___ = function(title, size){
-    var ctx, memo;
-    title == null && (title = '');
-    size == null && (size = 100);
-    ctx = getctx(size);
-    memo = [];
-    this.lift(function(it){
-      var x$, range, i$, len$, m, maxabs, i, y$, z$, z1$, x, h, y, z2$, z3$;
-      memo.splice(0, 0, it);
-      if (memo.length === 101) {
-        memo = memo.slice(0, 100);
-      }
-      x$ = ctx;
-      x$.fillStyle = '#000';
-      x$.fillRect(0, 0, 100, 100);
-      x$.font = '9px sans-serif';
-      x$.textBaseline = 'top';
-      if (_.isArray(it)) {
-        range = [[0, 0], [0, 0]];
-        for (i$ = 0, len$ = memo.length; i$ < len$; ++i$) {
-          m = memo[i$];
-          switch (false) {
-          case !(m[0] < range[0][0]):
-            range[0][0] = m[0];
-            break;
-          case !(m[0] > range[1][0]):
-            range[1][0] = m[0];
-            break;
-          case !(m[1] < range[0][1]):
-            range[0][1] = m[1];
-            break;
-          case !(m[1] > range[1][1]):
-            range[1][1] = m[1];
-          }
-        }
-        maxabs = maximum(map(abs, _.flatten(range)));
-        for (i$ = 0; i$ < 64; ++i$) {
-          i = i$;
-          if (memo[i] == null) {
-            continue;
-          }
-          ctx.strokeStyle = '#2' + (16 - Math.round(i / 4)).toString(16) + '2';
-          if (i === 0) {
-            ctx.strokeStyle = '#fff';
-          }
-          y$ = ctx;
-          y$.beginPath();
-          y$.moveTo(50.5, 50.5);
-          y$.lineTo(50.5 + memo[i][0] / (maxabs || 1) * 100, 50.5 - memo[i][1] / (maxabs || 1) * 100);
-          y$.stroke();
-        }
-        z$ = ctx;
-        z$.fillStyle = '#fff';
-        z$.fillText(title + '  ' + map(function(it){
-          return it.toFixed(3);
-        }, it).toString(), 0, 0);
-        return z$;
-      } else {
-        z1$ = ctx;
-        z1$.fillStyle = '#2f2';
-        maxabs = maximum(map(abs, memo));
-        for (i$ = 0, len$ = memo.length; i$ < len$; ++i$) {
-          i = i$;
-          m = memo[i$];
-          x = 99 - i;
-          h = 50 * abs(m) / (maxabs || 1);
-          if (m < 0) {
-            y = 50;
-          } else {
-            y = 50 - 50 * m / (maxabs || 1);
-          }
-          if (h === 0) {
-            h = 1;
-          }
-          z2$ = ctx;
-          z2$.fillRect(x, y, 1, h);
-        }
-        z3$ = ctx;
-        z3$.fillStyle = '#fff';
-        z3$.fillText(title + '  ' + it.toFixed(3).toString(), 0, 0);
-        return z3$;
-      }
-    });
-    return this;
-  };
-  inner = curry$(function(f, g, u, v){
-    return reduce(g, zipWith(f, u, v));
-  });
-  genOp = generalize_operator = function(name, fn){
-    var op;
-    return op = function(other){
-      var i$, len$, i, v, this$ = this;
-      if (other.length != null) {
-        zipWith;
-      }
-      for (i$ = 0, len$ = this.length; i$ < len$; ++i$) {
-        i = i$;
-        v = this[i$];
-        if (other[i].length != null) {
-          zipWith(it[name]);
-        }
-      }
-      return zipWith(function(it){
-        if (it.length != null) {
-          return it[name](this$);
-        } else {
-          return fn;
-        }
-      });
-    };
-  };
-  Tensor = (function(superclass){
-    var prototype = extend$((import$(Tensor, superclass).displayName = 'Tensor', Tensor), superclass).prototype, constructor = Tensor;
-    prototype['+'] = genOp('+', curry$(function(x$, y$){
-      return x$ + y$;
-    }));
-    prototype['-'] = genOp('-', curry$(function(x$, y$){
-      return x$ - y$;
-    }));
-    prototype['/'] = genOp('/', curry$(function(x$, y$){
-      return x$ / y$;
-    }));
-    prototype['*'] = genOp('*', curry$(function(x$, y$){
-      return x$ * y$;
-    }));
-    prototype['.'] = function(it){
-      return inner(curry$(function(x$, y$){
-        return x$ + y$;
-      }), curry$(function(x$, y$){
-        return x$ * y$;
-      }), this, it);
-    };
-    function Tensor(){
-      Tensor.superclass.apply(this, arguments);
-    }
-    return Tensor;
-  }(Array));
-  ref$ = Array.prototype;
-  ref$['+'] = function(it){
-    return zipWith(curry$(function(x$, y$){
-      return x$ + y$;
-    }), this, it);
-  };
-  ref$['-'] = function(it){
-    return zipWith(curry$(function(x$, y$){
-      return x$ - y$;
-    }), this, it);
-  };
-  ref$['/'] = function(it){
-    return zipWith(curry$(function(x$, y$){
-      return x$ / y$;
-    }), this, it);
-  };
-  ref$['*'] = function(it){
-    return zipWith(curry$(function(x$, y$){
-      return x$ * y$;
-    }), this, it);
-  };
-  ref$['.'] = function(it){
-    return inner(curry$(function(x$, y$){
-      return x$ + y$;
-    }), curry$(function(x$, y$){
-      return x$ * y$;
-    }), this, it);
-  };
-  BLOCKSIZE = 50;
   ACCEL = 0.01;
-  PLAYER_SIZE = [0.4, 1];
+  PLAYER_SIZE = [0.4, 0.6];
   GRAVITY = [0, -0.005];
-  MAP = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 1, 1, 1], [1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 1, 0, 1, 0], [0, 0, 1, 1, 1, 0, 0, 0]];
+  MAP = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 1, 1, 1], [1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 0, 0, 0, 0]];
   INITIAL_SPEED = [0, 0];
   INITIAL_POSITION = [0, 7];
-  box = function(x, y){};
-  draw = curry$(function(el, pos){
-    var ref$, x, y;
-    ref$ = map((function(it){
-      return it * BLOCKSIZE;
-    }), pos), x = ref$[0], y = ref$[1];
+  vadd = zipWith(curry$(function(x$, y$){
+    return x$ + y$;
+  }));
+  vsub = zipWith(curry$(function(x$, y$){
+    return x$ - y$;
+  }));
+  vlen = function(v){
+    return sqrt(Math.pow(v[0], 2) + Math.pow(v[1], 2));
+  };
+  vdot = function(a, b){
+    return fold(curry$(function(x$, y$){
+      return x$ + y$;
+    }), 0, zipWith(curry$(function(x$, y$){
+      return x$ * y$;
+    }), a, b));
+  };
+  vangle = function(a, b){
+    return vdot(a, b) / (vlen(a) * vlen(b));
+  };
+  vnorm = function(v){
+    return [v[0] / vlen(v), v[1] / vlen(v)];
+  };
+  draw = function(el, arg$, arg1$){
+    var x, y, ref$, sx, sy;
+    x = arg$[0], y = arg$[1];
+    ref$ = arg1$ != null
+      ? arg1$
+      : [1, 1], sx = ref$[0], sy = ref$[1];
     return $(el).css({
-      left: x,
-      top: 350 - y
+      left: x * 50,
+      top: 400 - sy * 50 - y * 50
     });
-  });
+  };
   $(function(){
     var i$, x, lresult$, j$, y, el, results$ = [];
     for (i$ = 0; i$ < 8; ++i$) {
@@ -217,63 +65,97 @@
     testWall = function(v, p, size){
       switch (false) {
       case !(v + p >= 8 - size):
-        return (8 - size) - p;
+        return (8 - size) - p - v;
       case !(v + p <= 0):
-        return -p;
+        return (0 - p) - v;
       default:
-        return v;
+        return 0;
       }
     };
     return zipAllWith(testWall, vel, pos, PLAYER_SIZE);
   };
-  collideBlocks = function(vel, pos){
-    var testBlock, blocks, i$, x, j$, y, len$, block;
-    testBlock = function(block, v, p){
-      return v['+p'];
+  collideBlocks = function(old_vel, cvel, pos){
+    var vel, testBlock, blocks, i$, x, j$, y, len$, block;
+    vel = cvel;
+    testBlock = function(block, size){
+      var testOverlap, overlap;
+      testOverlap = function(b, p, s){
+        var ref$;
+        switch (false) {
+        case !(b + 0.5 - s / 2 < p && p < b + 1):
+          return p - (b + 1);
+        case !(b + 0.5 + s / 2 > (ref$ = p + s) && ref$ > b):
+          return p + s - b;
+        default:
+          return 0;
+        }
+      };
+      overlap = zipAllWith(testOverlap, block, vadd(vel, pos), size);
+      if (overlap[0] !== 0 && overlap[1] !== 0) {
+        if (abs(vnorm(overlap)[1]) < abs(vnorm(vel)[1])) {
+          overlap[0] = 0;
+        } else {
+          overlap[1] = 0;
+        }
+        return vsub(vel, overlap);
+      } else {
+        return vel;
+      }
     };
     blocks = [];
     for (i$ = 0; i$ < 8; ++i$) {
       x = i$;
       for (j$ = 0; j$ < 8; ++j$) {
         y = j$;
-        if (MAP[y][y]) {
+        if (MAP[7 - y][x]) {
           blocks.push([x, y]);
         }
       }
     }
     for (i$ = 0, len$ = blocks.length; i$ < len$; ++i$) {
       block = blocks[i$];
-      zipAllWith(testBlock, block, vel, pos);
+      vel = testBlock(block, PLAYER_SIZE);
     }
-    return vel;
+    return vsub(vel, cvel);
   };
+  time = Time.frame();
   controls = Keyboard.arrows().lift(function(arg$){
     var x, y;
     x = arg$[0], y = arg$[1];
     return [x, y >= 0 ? y : 0];
   });
-  acceleration = controls.lift(map((function(it){
+  acceleration = controls.sampleOn(time).lift(map((function(it){
     return it * ACCEL;
   }))).lift(function(it){
-    return it['+'](GRAVITY);
-  }).__('A', 'accel');
-  acceleration.lift(function(it){
-    return it[1];
-  }).___('accel-y');
-  velocity = acceleration.sampleOn(Time.frame()).foldp(function(acc, old_vel){
-    var vel, cvel;
+    return vadd(it, GRAVITY);
+  });
+  velocity = acceleration.foldp(function(acc, old_vel){
+    var bvel, cvel, vvel;
     old_vel == null && (old_vel = INITIAL_SPEED);
-    vel = acc['+'](old_vel);
-    return cvel = collide(vel, (typeof position != 'undefined' && position !== null ? position._state : void 8) || INITIAL_POSITION);
-  }).__('V', 'velocity').___('velocity');
-  velocity.lift(function(it){
-    return it[1];
-  }).___('velocity-y');
+    bvel = vadd(old_vel, acc);
+    cvel = vadd(bvel, collide(bvel, (typeof position != 'undefined' && position !== null ? position._state : void 8) || INITIAL_POSITION));
+    return vvel = vadd(cvel, collideBlocks(old_vel, cvel, (typeof position != 'undefined' && position !== null ? position._state : void 8) || INITIAL_POSITION));
+  });
   position = velocity.foldp(function(vel, pos){
     pos == null && (pos = INITIAL_POSITION);
-    return vel['+'](pos);
-  }).__('P', 'position');
-  player = position.lift(draw('.player'));
+    return vadd(vel, pos);
+  });
+  collision = Lift(function(pos, vel){});
+  position.lift(function(it){
+    return draw('.player', it, PLAYER_SIZE);
+  });
+  position.___('position').__('P', 'position');
+  x$ = velocity;
+  x$.___('velocity');
+  x$.__('V', 'velocity');
+  x$.lift(function(it){
+    return it[1];
+  }).___('velocity-y');
+  x$.lift(function(it){
+    return it[0];
+  }).___('velocity-x');
+  acceleration.__('A', 'accel').___('accel');
+  controls.__('C', 'controls').___('controls');
   function import$(obj, src){
     var own = {}.hasOwnProperty;
     for (var key in src) if (own.call(src, key)) obj[key] = src[key];
@@ -291,11 +173,5 @@
       } : f;
     };
     return _curry();
-  }
-  function extend$(sub, sup){
-    function fun(){} fun.prototype = (sub.superclass = sup).prototype;
-    (sub.prototype = new fun).constructor = sub;
-    if (typeof sup.extended == 'function') sup.extended(sub);
-    return sub;
   }
 }).call(this);
--- game.ls
+++ game.ls
@@ -2,113 +2,9 @@ import furp
 __ = -> console.log ...arguments;  it
 
 
-getctx = (size) ->
-  if not $ '#__signals' .0 then $ \body .append "<div id='__signals' style='position:fixed;height:100%;width:100%;'></div>"
-  $ '#__signals' .append el = $ "<canvas height='#{size}' width='#{size}' style='float:right;top:0;left:0;height:#{size}px;width:#{size}px;'>"
-  ctx = el[0].getContext '2d'
-
-
-furp.SignalClass.prototype.___= (title = '', size=100) -> # log helper
-  ctx = getctx size
-  memo = []
-  @lift ->
-    memo.splice 0, 0, it
-    if memo.length == 101
-      memo := memo.slice 0,100
-    #scale =
-    ctx
-      ..fillStyle = \#000
-      ..fillRect 0,0, 100, 100
-      ..font = '9px sans-serif'
-      ..textBaseline = 'top'
-
-    #__ it[1]*200
-
-    if _.isArray it
-      range = [[0 0] [0 0]]
-      for m in memo => switch
-        | m.0 < range.0.0 => range.0.0 = m.0
-        | m.0 > range.1.0 => range.1.0 = m.0
-        | m.1 < range.0.1 => range.0.1 = m.1
-        | m.1 > range.1.1 => range.1.1 = m.1
-      #__ range
-      #range = [[(minimum map (.0) memo), (minimum map (.1) memo)],
-      #         [(maximum map (.0) memo), (maximum map (.1) memo)]]
-
-      maxabs = maximum map abs, _.flatten range
-      for i from 0 til 64
-        if not memo[i]? then continue
-        #__ memo[i]
-        ctx.strokeStyle = '#2'+ (16-(Math.round i/4)).toString(16) + '2'
-        if i == 0
-          ctx.strokeStyle = '#fff'
-        ctx
-          #..lineWidth = 2
-          ..beginPath!
-          ..moveTo 50.5, 50.5
-          ..lineTo 50.5 + memo[i][0] / (maxabs or 1)*100, 50.5 - memo[i][1]/(maxabs or 1)* 100
-          ..stroke!
-      ctx
-        ..fillStyle = \#fff
-        ..fillText(title + '  ' + (map (.toFixed 3), it).toString!, 0, 0);
-    else
-      ctx
-        ..fillStyle = \#2f2
-      maxabs = maximum map abs, memo
-      for m, i in memo
-        #__ m[0], i
-        x = 99-i
-        h = (50 * (abs m) / (maxabs or 1))
-        if m < 0 => y = 50
-        else y = 50-(50*m/(maxabs or 1));
-        if h == 0 then h = 1
-        ctx
-          ..fillRect x, y, 1, h
-      ctx
-        ..fillStyle = \#fff
-        ..fillText(title + '  ' + it.toFixed(3).toString(), 0, 0);
-
-  this
-
-
-inner = (f, g, u, v) --> reduce g, zipWith f, u, v
-
-
-gen-op = generalize_operator = (name, fn) -> op = (other) ->
-  if other.length?
-    zipWith
-  for v, i in @
-    if other[i].length?
-      zipWith it[name]
-  zipWith ~> if it.length? then it[name] @ else fn
-
-
-class Tensor extends Array
-  \+ : gen-op \+, (+)
-  \- : gen-op \-, (-)
-  \/ : gen-op \/, (/)
-  \* : gen-op \*, (*)
-  \. : -> inner (+), (*), @, it
-
-
-#Array.prototype <<< Tensor.prototype
-Array.prototype <<<
-  \+ : -> zipWith (+), @ , it
-  \- : -> zipWith (-), @ , it
-  \/ : -> zipWith (/), @ , it
-  \* : -> zipWith (*), @ , it
-  \. : -> inner (+), (*), @, it
-
-
-
-
-
-
-BLOCKSIZE = 50
 ACCEL = 0.01
-PLAYER_SIZE = [0.4 1]
+PLAYER_SIZE = [0.4 0.6]
 GRAVITY = [0 -0.005]
-
 
 MAP =[[0 0 0 0 0 0 0 0]
       [0 0 1 1 0 0 0 0]
@@ -116,18 +12,22 @@ MAP =[[0 0 0 0 0 0 0 0]
       [0 0 0 0 1 1 1 1]
       [1 1 0 0 0 0 0 0]
       [0 0 0 0 0 0 1 0]
-      [0 0 0 0 1 0 1 0]
-      [0 0 1 1 1 0 0 0]]
+      [0 0 0 0 0 0 0 0]
+      [0 1 0 1 0 0 0 0]]
 
 INITIAL_SPEED = [0 0]
 INITIAL_POSITION = [0 7]
 
 
-box = (x, y) ->
+vadd = zipWith (+)
+vsub = zipWith (-)
+vlen = (v) -> sqrt v.0 ^ 2 + v.1 ^ 2
+vdot = (a, b) -> fold (+), 0, zipWith (*), a, b
+vangle = (a, b) -> (vdot a, b) / ((vlen a) * (vlen b))
+vnorm = (v) -> [(v.0 / vlen v), (v.1 / vlen v)]
 
-draw = (el, pos) -->
-  [x, y] = map (*BLOCKSIZE), pos
-  $ el .css {left: x, top: 350-y}
+draw = (el, [x,y], [sx, sy]=[1 1]) ->
+  $ el .css {left: x*50, top: 400 - sy*50 - y*50}
 
 $ ->
   #__ MAP
@@ -139,63 +39,98 @@ $ ->
 
 collide = (vel, pos) ->
   test-wall = (v, p, size) ->
-    | v+p >= 8-size => (8-size)-p
-    | v+p <= 0        => -p
-    | _               => v
+    | v+p >= 8 - size => (8-size)-p - v
+    | v+p <= 0        => (0-p) - v
+    | _               => 0
   zipAllWith test-wall, vel, pos, PLAYER_SIZE
 
+collide-blocks = (old_vel, cvel, pos) ->
+  vel = cvel
+  test-block = (block, size) ->
+    test-overlap = (b, p, s) ->
+      | b + 0.5 - s/2 < p     < b + 1  => p     - (b + 1)
+      | b + 0.5 + s/2 > p + s > b      => p + s -  b
+      | _ => 0
 
-collide-blocks = (vel, pos) ->
-  test-block = (block, v, p) ->
-    v\+p
-    #min
-    #| v+p
+    overlap = zipAllWith test-overlap, block, (vadd vel, pos), size
+    if (overlap.0 != 0) and (overlap.1 != 0)
+      #__ vel, overlap
+      #__ (vnorm overlap), (vnorm vel)
+      if (abs (vnorm overlap).1) < (abs (vnorm vel).1)
+        overlap[0] = 0
+      else
+        overlap[1] = 0
+      #if (abs overlap.1) >= (abs vel.1)
+      #  overlap[1] = 0
+      #else if (abs overlap.0) >= (abs vel.0)
+      #  overlap[0] = 0
+      vsub vel, overlap
+    else
+      vel
 
   blocks = []
   for x til 8
     for y til 8
-      blocks.push [x, y] if MAP[y][y]
+      blocks.push [x, y] if MAP[7-y][x]
 
   for block in blocks
-    zipAllWith test-block, block, vel, pos
+    vel = test-block block, PLAYER_SIZE
+  vsub vel, cvel
 
-  vel
 
+
+time = Time.frame!
+  #.keep-when Keyboard.isDown 32
 
 controls = Keyboard.arrows!
   .lift ([x,y]) -> [x, (if y >= 0 then y else 0)]
 
-acceleration = controls
-  .lift map (* ACCEL)
-  .lift -> it\+ GRAVITY
-  .__ 'A', 'accel'
 
-acceleration
-  .lift -> it[1]
-  .___ 'accel-y'
+acceleration = controls
+  .sample-on time
+  .lift map (* ACCEL)
+  .lift -> vadd it, GRAVITY
+  #.control (-> collision), acc,
+
+
 
 velocity = acceleration
-  .sample-on Time.frame!
-  #.control (-> position), (acc, pos) ->
-  #  vel = acc\+ old_vel
-  #  cvel = collide vel, position?_state || INITIAL_POSITION
+  #.control (-> position), (acc, pos, vel) ->
+  #  vel = vadd acc, old_vel
+  #  collide (acc\ +vel), INITIAL_POSITION#pos || INITIAL_POSITION
   .foldp (acc, old_vel = INITIAL_SPEED) ->
-    vel = acc\+ old_vel
-    cvel = collide vel, position?_state || INITIAL_POSITION
-  #  bvel = collide-blocks cvel, position?_state || INITIAL_POSITION
-  .__ 'V', 'velocity'
-  .___ 'velocity'
+    bvel = (vadd old_vel, acc)
+    cvel = vadd bvel, (collide bvel, position?_state || INITIAL_POSITION)
+    vvel = vadd cvel, collide-blocks old_vel, cvel, position?_state || INITIAL_POSITION
 
-velocity
-  .lift -> it[1]
-  .___ 'velocity-y'
 
 position = velocity
-  .foldp (vel, pos = INITIAL_POSITION) -> vel\+ pos
+  .foldp (vel, pos = INITIAL_POSITION) -> vadd vel, pos
+
+
+collision = Lift (pos, vel) ->
+
+
+
+
+position.lift -> draw \.player, it, PLAYER_SIZE
+
+position
+  .___ 'position'
   .__ 'P', 'position'
 
-player = position
-  #.lift __
-  .lift draw \.player
+velocity
+  ..___ 'velocity'
+  ..__ 'V', 'velocity'
+  ..lift -> it[1]
+    .___ 'velocity-y'
+  ..lift -> it[0]
+    .___ 'velocity-x'
 
+acceleration
+  .__ 'A', 'accel'
+  .___ 'accel'
 
+controls
+  .__ 'C', 'controls'
+  .___ 'controls'
